a754552b30448aaf2298d683708de49bacaf9378
Gemma Rate <thebrickmaster@theratefamily.co.uk>
2020-11-29 18:36:15 +0000

    Removed reference to DR2 data in description.

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setuptools.setup(
     entry_points ={'console_scripts': ['wrdists = wrdists.console_access:main']},
     version='1.1',
     license='MIT',
-    description='Calculates Galactic WR distances from Gaia DR2 parallaxes',
+    description='Calculates Galactic WR distances from Gaia parallaxes',
     long_description=open('README.md').read(),
 )
